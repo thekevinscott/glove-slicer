d6eaa6c049b6e54cbb3c0032454c4cfbf391ed4b
Kevin Scott <kevin@hitheory.com>
2018-09-26 14:44:34 -0400

    Ignore data and update slice tool

--- slice.py
+++ slice.py
@@ -1,1 +1,14 @@
-print('hi')
+import argparse
+import os
+
+parser=argparse.ArgumentParser()
+
+parser.add_argument('--file', help='The file containing the word embeddings, separated by newlines')
+parser.add_argument('--chunk_size', help='The number of lines per chunk', default=5)
+
+args=parser.parse_args()
+
+if not os.path.isfile(args.file):
+    raise Exception("File could not be found")
+
+print(args.chunk_size)
